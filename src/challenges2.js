3d0bb65e444b6743e5e8186e75bb2641bc7c0aba
fortunatozm <fortunatompongo@hotmail.com>
2021-12-05 16:17:04 -0300

    Requsito 13 - OK

--- src/challenges2.js
+++ src/challenges2.js
@@ -52,7 +52,34 @@ function triangleCheck(lineA, lineB, lineC) {
 }
 
 // Desafio 13
-function hydrate() {
+function hydrate(textReceived) {
+
+  let arrayStr = textReceived.match(/\d/g); //  match(/\d/g) - Função que pega numeros de uma string. Foi extraído do Stack Overflow (https://pt.stackoverflow.com/questions/3719/como-obter-apenas-os-n%C3%BAmeros-de-uma-string-em-javascript)
+
+  let arrayNumber = [];
+
+  for (let index = 0; index < arrayStr.length; index++) {
+    
+    arrayNumber[index] = parseInt(arrayStr[index]);
+    
+  }
+
+  let soma = 0;
+
+  for (let index = 0; index < arrayNumber.length; index++) {
+    
+    soma = soma + arrayNumber[index];
+    
+  }
+
+  if(soma <= 1){
+    return soma + " copo de água";
+  }
+
+  if(soma > 1){
+  return soma + " copos de água";
+  }
+
   // seu código aqui
 }
 
